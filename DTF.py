fc78dca58d67ee3539e1699afe035c4d6aa2650d
Sergey <sergey3010.01@mail.ru>
2023-02-04 01:56:13 +0300

    tips and algo

--- DTF.py
+++ DTF.py
@@ -1,10 +1,19 @@
 import requests
-from fastapi import FastAPI
+
+email = "sergey.sysoev3010@gmail.com"
+pswrd = "CheckPassword" 
+
+url_login = 'https://api.dtf.ru/v3.0/auth/simple/login'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/109.0'
+}
+
+# tip : https://gadjimuradov.ru/post/python-requests-avtorizaciya-na-sajte/
+# tip2 : https://newtechaudit.ru/parsing-sajta-s-primeneniem-avtorizaczii/
 
 class DTF:
 	def __init__(self):
-		self.__fastapi = FastAPI()
+		
 
-	def __make_request(self):
-		pass
-    
+    def __make_request(self):
+		
