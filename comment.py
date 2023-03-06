db97bed5e9864a2dd40c011be6c1c8cc6789232c
Сергей Сысоев <bleaf@MacBook-Pro-Sergej.local>
2023-03-06 16:54:19 +0300

    commentTree

--- comment.py
+++ comment.py
@@ -1,3 +1,12 @@
+class CommentTree:
+    def __init__(self, comments_list :list, entry_id :int) -> None:
+        self._comments_list = comments_list
+        self._entry_id = entry_id
+    
+    def make_full_tree():
+        ...
+        
+
 class Comment:
     def __init__(self, id, auth_name, media, reply_to, text, lvl):
         self._id = id
@@ -8,11 +17,11 @@ class Comment:
         self._level = lvl
         self.time = 0
         self.answers = []
-    def __str__(self):
+    def __str__(self) -> str:
         """Trail version of this method"""
         return f"{self._author_name}: {self._text} <|endofstatement|>"
     
-    def get_answers(self):
+    def get_answers(self) -> list:
         return self.answers.copy()
     
     # def to_send(self):
